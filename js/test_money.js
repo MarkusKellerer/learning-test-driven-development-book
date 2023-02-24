ec183684fed810f1d36201f1853eae8542279e37
Markus Kellerer <markus.kellerer@outlook.com>
2023-02-24 10:54:57 +0200

    feat: Multiply in Eur

--- js/test_money.js
+++ js/test_money.js
@@ -1,17 +1,24 @@
 const assert = require('assert');
 
-class Dollar {
+class Money {
 
-    constructor(amount) {
+    constructor(amount, currency) {
         this.amount = amount
+        this.currency = currency
     }
 
     times(multiplier) {
-        return new Dollar(this.amount * multiplier)
+        return new Money(this.amount * multiplier, this.currency)
     }
 }
 
-let fiver = new Dollar(5);
+let fiver = new Money(5, "USD");
 let tenner = fiver.times(2);
 
 assert.strictEqual(tenner.amount, 10);
+assert.strictEqual(tenner.currency, "USD");
+
+let tenEuro = new Money(10, "EUR");
+let twentyEuro = tenEuro.times(2)
+assert.strictEqual(twentyEuro.amount, 20);
+assert.strictEqual(twentyEuro.currency, "EUR");
